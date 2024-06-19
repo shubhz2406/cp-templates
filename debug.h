b4b465f93bda74141afe881a2df38fa89a1b0773
sharma_shubh <meolessi6@gmail.com>
2024-06-19 19:54:22 +0530

    debug and sol1 modified

---
+++ debug.h
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+ 
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
--- sol1.cpp
+++ sol1.cpp
@@ -1,105 +1,58 @@
- // //
-#include<bits/stdc++.h>
+   #include<bits/stdc++.h>
+   #ifndef ONLINE_JUDGE
+   #include "debug.h"
+   #else
+   #define debug(x)  
+   #endif
 
-using namespace std;
-using namespace chrono;
+   using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
+   #define MOD 1000000007
+   #define MOD1 998244353
+   #define INF 1e18
+   #define nline "\n"
+   #define pb push_back
+   #define ppb pop_back
+   #define mp make_pair
+   #define ff first
+   #define ss second
+   #define sz(x) ((int)(x).size())
+   #define all(x) (x).begin(), (x).end()
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
+   typedef long long ll;
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-
-
-void solve()
-{
-  
-   int tt;
-   cin >> tt;
-   while(tt--)
+   void solve()
    {
-      debug(tt)
+     
+      int tt;
+      cin >> tt;
+      while(tt--)
+      {
+         debug(tt)
+         
+
+
+
+      }
       
 
-      
    }
-   
-
-}
 
 
-int main()
-{
-
- 
-
-   #ifndef ONLINE_JUDGE
-      freopen("Error.txt", "w", stderr);
-      freopen("input.txt", "r", stdin);
-      freopen("output.txt", "w", stdout);
-   #endif
-
-   fastio();
-   auto start1 = high_resolution_clock::now();
-//___________________________________________________________________________________________
-
-   
-   solve();
-
-
-//____________________________________________________________________________________________
-   auto stop1 = high_resolution_clock::now();
-   auto duration = duration_cast<microseconds>(stop1 - start1);
-   #ifndef ONLINE_JUDGE
-      cerr << "Time: " << duration . count() / 1000 << endl;
-   #endif
-   
- 
-
-   return 0;
- 
-} 
+   int main()
+   {
+      #ifndef ONLINE_JUDGE
+         freopen("Error.txt", "w", stderr);
+         freopen("input.txt", "r", stdin);
+         freopen("output.txt", "w", stdout);
+      #endif
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+      cout.tie(NULL);
+      
+      solve();
+      return 0;
+    
+   } 
 
 
